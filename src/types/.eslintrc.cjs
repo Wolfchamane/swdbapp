a2b8b95324dcd53a5e1b14e351710d358006601b
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-13 20:59:07 +0100

    feature (/src/types): added general types package

---
+++ src/types/.eslintrc.cjs
@@ -0,0 +1,5 @@
+/* eslint-env node */
+module.exports = {
+	root: true,
+	...require('@swdbapp/configurations/eslint.cjs'),
+};
---
+++ src/types/.prettierrc.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+	...require('@swdbapp/configurations/prettier.cjs'),
+};
