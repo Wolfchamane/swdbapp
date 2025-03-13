ff83cc66ff8aacc50f8f0ab50e5c7ecb6f3d356f
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-13 20:58:42 +0100

    feature (/src/*): moved app solutions to specific source folder

---
+++ src/apps/vuejs/src/components/app-wrapper.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+	import { type Ref, onMounted, ref } from 'vue';
+	import { providePeopleUseCases } from '@swdbapp/feature-people';
+
+	const useCases = providePeopleUseCases();
+	const response: Ref<string> = ref('');
+
+	onMounted(async () => {
+		response.value = JSON.stringify(await useCases.list(), null, 2);
+	});
+</script>
+
+<template>
+	<header class="app-header"></header>
+	<main class="app-main">
+		<h1>It works!</h1>
+		<p>{{ response }}</p>
+	</main>
+	<footer class="app-footer"></footer>
+</template>
--- src/apps/vuejs/src/main.ts
+++ src/apps/vuejs/src/main.ts
@@ -1,9 +1,9 @@
-import '../../styles/main.sass';
 import { createApp } from 'vue';
+import '../../../styles/main.sass';
 import AppWrapper from '@/components/app-wrapper.vue';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const app = createApp(AppWrapper);
+	const app = createApp(AppWrapper);
 
-    app.mount('#app');
+	app.mount('#app');
 });
