8e7f8c411435d567d8363af1a5396c5997f4e274
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-14 17:13:09 +0100

    feature (/features/people): re-organized content

---
+++ src/features/people/graph.ts
@@ -0,0 +1,14 @@
+import {
+    DefaultFilmsHttpClient,
+    DefaultPeopleHttpClient,
+    type FilmsHttpClient,
+    type PeopleHttpClient,
+} from '@swdbapp/infra-http';
+import { PeopleHttpAdapter } from './adapters/output';
+import { DefaultPeopleUseCases, type PeoplePorts, PeopleUseCases } from './application';
+
+const peopleHttpClient: PeopleHttpClient = new DefaultPeopleHttpClient();
+const filmsHttpClient: FilmsHttpClient = new DefaultFilmsHttpClient();
+const peopleHttpAdapter: PeoplePorts = new PeopleHttpAdapter(peopleHttpClient, filmsHttpClient);
+
+export const providePeopleUseCases = (): PeopleUseCases => new DefaultPeopleUseCases(peopleHttpAdapter);
