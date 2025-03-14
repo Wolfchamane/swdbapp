39ede7db52f98aa620c2f70c13d8e2f6fd4cf4e2
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-14 17:13:34 +0100

    feature (/features/people): export people-view for vue

---
+++ src/features/people/adapters/views/index.ts
@@ -0,0 +1,5 @@
+import VuePeopleView from './vue-js/people-view.vue';
+
+export const PEOPLE_FEATURE_VIEWS = {
+    vueJs: VuePeopleView,
+};
---
+++ src/features/people/adapters/views/vue-js/people-view.vue
@@ -0,0 +1,7 @@
+<script lang='ts' setup></script>
+
+<template>
+    <div class='people-view'>
+        <h1>People View</h1>
+    </div>
+</template>
