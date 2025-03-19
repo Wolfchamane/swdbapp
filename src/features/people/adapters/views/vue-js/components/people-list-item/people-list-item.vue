0cf199819bfc3cf2667074d86e3097cb7e0bc35f
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-19 17:25:38 +0100

    feature (/features/people/**/people-list-item.vue): navigate to character detail view on click

--- src/features/people/adapters/views/vue-js/components/people-list-item/people-list-item.vue
+++ src/features/people/adapters/views/vue-js/components/people-list-item/people-list-item.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
+	import { useRouter } from 'vue-router';
 	import type { PeopleListItemProperties } from './people-list-item.types';
 
-	defineProps<PeopleListItemProperties>();
+	const { id, name, alignment } = defineProps<PeopleListItemProperties>();
+	const router = useRouter();
+
+	const onItemClick = () => {
+		router.push({ path: `/people/${id}` });
+	};
 </script>
 
 <template>
 	<div
+		@click.stop="onItemClick"
 		:class="`people-list-item d-flex align-center justify-space-between my-1 p-2 pt-1 pb-1 w-100 border-1 radius-s ${alignment}`">
 		<span class="people-list-item__name grow">{{ name }}</span>
 		<div :class="`people-list-item__lightSaber fs-m`">
