3b87db494ea2c5e2b21c3b54c763f50ece413656
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-20 16:52:40 +0100

    feature (characters/graph.ts): inject API configuration into httpClient

--- src/features/characters/graph.ts
+++ src/features/characters/graph.ts
@@ -1,10 +1,9 @@
-import { provideAPIHostname } from '@swdbapp/core';
+import { provideAPIConfiguration } from '@swdbapp/core';
 import { type CharactersHttpClient, DefaultCharactersHttpClient } from '@swdbapp/infra-http';
 import { CharactersHttpAdapter } from './adapters/output/characters.http-adapter';
 import { type CharactersPorts, type CharactersUseCases, DefaultCharactersUseCases } from './application';
 
-const hostname: string = provideAPIHostname();
-const httpClient: CharactersHttpClient = new DefaultCharactersHttpClient({ hostname });
+const httpClient: CharactersHttpClient = new DefaultCharactersHttpClient(provideAPIConfiguration());
 const httpAdapter: CharactersPorts = new CharactersHttpAdapter(httpClient);
 
 let charactersUseCasesSingleton: CharactersUseCases | undefined;
