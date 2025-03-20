bd0122a888a9bab7f521b3428b14c726c60f00a7
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-20 16:52:15 +0100

    feature (core/**/*.ts): retrieve environment configuration from global scope

--- src/features/core/src/app-config.ts
+++ src/features/core/src/app-config.ts
@@ -1,7 +1,32 @@
-import type { AppWindow, SWDBAppConfiguration } from './types';
+import type { AppWindow, SWDBAppConfiguration, APIConfiguration } from './types';
 
 export const provideAPIHostname = (): string => {
 	const w: AppWindow = window as AppWindow;
 
 	return w && 'swdbapp' in w ? (w.swdbapp as SWDBAppConfiguration).apiHostname : '';
+};
+
+export const isAPISecureProtocol = (): boolean => {
+    const w: AppWindow = window as AppWindow;
+
+    return w && 'swdbapp' in w ? (w.swdbapp as SWDBAppConfiguration).apiProtocol === 'https' : false;
+};
+
+export const provideAPIPort = (): string => {
+    const w: AppWindow = window as AppWindow;
+
+    return w && 'swdbapp' in w ? (w.swdbapp as SWDBAppConfiguration).apiPort : '';
+};
+
+export const provideAPIConfiguration = (): APIConfiguration => {
+    let port: string | number | undefined = provideAPIPort();
+    if (port !== undefined) {
+        port = Number(port)
+    }
+
+    return {
+        hostname: provideAPIHostname(),
+        secure: isAPISecureProtocol(),
+        port,
+    }
 };
--- src/features/core/src/types.ts
+++ src/features/core/src/types.ts
@@ -1,5 +1,13 @@
 export interface SWDBAppConfiguration {
 	apiHostname: string;
+    apiProtocol: string;
+    apiPort: string;
 }
 
 export type AppWindow = Window & typeof globalThis & { swdbapp: SWDBAppConfiguration };
+
+export interface APIConfiguration {
+    hostname: string;
+    secure: boolean;
+    port?: number;
+}
