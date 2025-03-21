d7811b0b9f537e36fdecde5982ea9efbfe9c938f
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-21 09:14:01 +0100

    feature (*): improve characters view

--- src/features/characters/adapters/views/vue-js/characters-view.vue
+++ src/features/characters/adapters/views/vue-js/characters-view.vue
@@ -14,16 +14,17 @@
 	const fetchCharacters = async (): Promise<void> => {
 		isLoading.value = true;
 		try {
+			characters.value = new Array(10);
 			const list = await useCases.list({
 				page: page.value,
 			});
-			totalPages.value = Number(list.total / list.limit);
+			totalPages.value = Math.ceil(Number(list.total / list.limit));
 			currentPage.value = list.page;
 			characters.value = list.items;
 		} catch (e) {
 			console.log(e);
 		} finally {
-			isLoading.value = false;
+			setTimeout(() => (isLoading.value = false), 400);
 		}
 	};
 
@@ -43,11 +44,23 @@
 		<pagination-control :current-page="currentPage" :total-pages="totalPages" :min="1" @paginate="onPaginate" />
 		<div class="characters-view d-flex flex-wrap justify-center w-100 h-100 p-1 overflow-hidden overflow-y-auto">
 			<div
-				v-for="character in characters"
-				:key="`character-${character.$id}`"
-				class="characters-view__item d-block p-relative radius-xs overflow-hidden">
-				<img class="characters-view__item-picture" :src="character.image.href" :alt="character.name" />
-				<span class="characters-view__item-name p-absolute left-0 bottom-0">{{ character.name }}</span>
+				v-for="(character, index) in characters"
+				:key="`character-${(character && character.$id) || index}`"
+				:class="[
+					'characters-view__item',
+					'd-block',
+					'p-relative',
+					'radius-xs',
+					'overflow-hidden',
+					{
+						'characters-view__item--loading': isLoading,
+					},
+				]">
+				<div class="characters-view__item-loading-veil d-block p-absolute z-1 top-0 left-0 w-100 h-100"></div>
+				<template v-if="!isLoading && character">
+					<img class="characters-view__item-picture" :src="character.image.href" :alt="character.name" />
+					<span class="characters-view__item-name p-absolute left-0 bottom-0">{{ character.name }}</span>
+				</template>
 			</div>
 		</div>
 	</div>
