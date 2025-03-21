7767eadbb62ccfffe44bd13efcde88f83c94d841
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-21 11:00:30 +0100

    feature (app-wrapper.vue): + improve header + show license

--- src/apps/vue-js/src/components/app-wrapper.vue
+++ src/apps/vue-js/src/components/app-wrapper.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
 	import { capitalize } from '@amjs/js-utils';
-	import { faCircleLeft } from '@fortawesome/free-solid-svg-icons';
+	import { faCircleLeft, faGear, faCircleInfo, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 	import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-	import { type ComputedRef, computed } from 'vue';
-	import { useRoute, useRouter } from 'vue-router';
+	import { type Ref, ref, type ComputedRef, computed } from 'vue';
+	import { RouterView, useRoute, useRouter } from 'vue-router';
+    import LICENSE from '../../../../../LICENSE?raw';
 
 	const ROUTER = useRouter();
 	const currentRoute = useRoute();
+    const showLicense: Ref<boolean> = ref(false);
+
 	const isHomeView: ComputedRef<boolean> = computed(() => {
 		return currentRoute.name === 'home-view';
 	});
+
+    const toggleLicense = () => showLicense.value = !showLicense.value;
 
 	const navigateBack = () => {
 		ROUTER.back();
@@ -17,17 +22,21 @@
 </script>
 
 <template>
-	<header class="app-header border-bottom-1 center">
+	<header class="app-header d-flex align-center border-bottom-1 center color-primary py-1">
+        <font-awesome-icon v-if="!isHomeView" :icon="faCircleLeft" @click="navigateBack" class="mx-1 cursor-pointer" />
 		<template v-if="isHomeView">
-			<h1 class="sw-ff color-primary">Star Wars</h1>
-			<p class="sw-ff color-primary">DB Application</p>
+            <div class='d-flex flex-column center grow'>
+                <span class="sw-ff color-primary m-0 my-1 fs-xl">Star Wars</span>
+                <span class="sw-ff color-primary m-0 mb-05">Explorer</span>
+            </div>
 		</template>
 		<template v-else>
-			<div class="d-flex align-center py-1 fs-xl color-primary">
-				<font-awesome-icon :icon="faCircleLeft" @click="navigateBack" class="mx-1" />
-				<div class="grow sw-ff">{{ capitalize(String(currentRoute.name).replace('-view', '')) }}</div>
-			</div>
+            <div class="grow sw-ff">{{ capitalize(String(currentRoute.name).replace('-view', '')) }}</div>
 		</template>
+        <div class='d-flex align-center justify-space-between px-1'>
+            <font-awesome-icon :icon='faCircleInfo' class='mr-1 cursor-pointer' @click='toggleLicense'/>
+            <font-awesome-icon v-if='!isHomeView' :icon='faGear' class="disabled" />
+        </div>
 	</header>
 	<main class="app-main grow overflow-y-auto">
 		<router-view v-slot="{ Component }">
@@ -36,7 +45,21 @@
 			</transition>
 		</router-view>
 	</main>
-	<footer class="app-footer d-flex align-center justify-center p-1 border-top-1">About</footer>
+    <transition name='fade' appear>
+	    <footer v-if="isHomeView" class="app-footer d-flex align-center justify-center p-1 border-top-1">
+            &copy; 2025 - Arturo Martínez Díaz
+        </footer>
+    </transition>
+    <transition name='slide'>
+        <div class='app-license d-flex flex-column p-absolute top-0 left-0 z-1 w-100 h-100 m-1 p-1 radius-m bg-color-background color-foreground' v-show='showLicense'>
+            <font-awesome-icon :icon='faCircleXmark' @click='toggleLicense' class="cursor-pointer" style='align-self: end'/>
+            <pre v-html='LICENSE' class='grow ta-justify'/>
+        </div>
+    </transition>
 </template>
 
-<style lang="sass"></style>
+<style lang="sass">
+.app-license
+    width: calc(100% - 2em) !important
+    height: calc(100% - 2em) !important
+</style>
