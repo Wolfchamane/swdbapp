1b4b521b3164bc00b64ebb756a5f5867dc66823e
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-28 20:49:10 +0100

    feature (/vue-js/routes/**/*.ts): new route for character details

--- src/apps/vue-js/src/router/routes/index.ts
+++ src/apps/vue-js/src/router/routes/index.ts
@@ -1,4 +1,5 @@
+import characterDetailsRoute from './character-details.route';
 import charactersRoute from './characters.route';
 import homeRoute from './home.route';
 
-export default [homeRoute, charactersRoute];
+export default [homeRoute, charactersRoute, characterDetailsRoute];
