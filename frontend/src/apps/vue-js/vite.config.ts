002ad5d347698450974bc08b95d4d1f0909b393f
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-31 13:16:23 +0200

    chore (*): move "src" into "frontend" folder

---
+++ frontend/src/apps/vue-js/vite.config.ts
@@ -0,0 +1,37 @@
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import svg from 'vite-svg-loader';
+
+const publicDir: string = path.resolve(
+	...[fileURLToPath(new URL('./', import.meta.url)), '..', '..', '..', '..', 'www', 'software', 'swdbapp', 'assets']
+);
+
+const outDir: string = path.resolve(
+	...[fileURLToPath(new URL('./', import.meta.url)), '..', '..', '..', '..', 'www', 'software', 'swdbapp', 'vue-js']
+);
+
+const envDir: string = path.resolve(...[fileURLToPath(new URL('./', import.meta.url)), '..', '..', '..', '..']);
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+	const isPro: boolean = /^pro/g.test(mode);
+
+	return {
+		base: isPro ? '/software/swdbapp/vue-js' : './',
+		plugins: [vue(), vueDevTools(), svg()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		envDir,
+		publicDir: isPro ? false : publicDir,
+		build: {
+			outDir,
+			emptyOutDir: true,
+		},
+	};
+});
