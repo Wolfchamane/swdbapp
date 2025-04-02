cf011cfb1bf622dbca44a70c7271879dfa3d242e
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-02 12:57:08 +0200

    feature (db/scripts/02.titles.create.sql): update with full structure

--- db/scripts/02.titles.create.sql
+++ db/scripts/02.titles.create.sql
@@ -3,7 +3,20 @@ DROP TABLE IF EXISTS "titles";
 CREATE TABLE "titles" (
     "id" serial NOT NULL,
     "title" VARCHAR(512) UNIQUE NOT NULL,
-    "logo"	VARCHAR(512)
+    "logo"	VARCHAR(512),
+    "type"  VARCHAR(16),
+    "poster" VARCHAR(512),
+    "rating" VARCHAR(16),
+    "duration" VARCHAR(13),
+    "release_date" VARCHAR(32),
+    "genre" VARCHAR(128),
+    "resume" TEXT,
+    "director" VARCHAR(128),
+    "music_director" VARCHAR(128),
+    "producers" VARCHAR(512),
+    "actors" VARCHAR(1024),
+    "plot" TEXT,
+    "opening_crawl" TEXT
 );
 
 ALTER TABLE "titles"
