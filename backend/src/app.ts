7feeb0b99c2f0102857ba9da4b5ab5d2e85238a4
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-05 09:17:55 +0200

    feature (/backend/src/**/*.ts): new auth middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { cors, json, endHandler } from './middlewares';
+import { auth, cors, json, endHandler } from './middlewares';
 import provideRouter from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(auth());
 app.use(provideRouter());
 app.use(endHandler());
 
