5f1609af1e633e66080dab6b8ecfdd3c30593e7f
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-07 01:15:50 +0200

    feature (/feature/core/**/*.ts): add solution for assets

---
+++ features/core/src/assets.ts
@@ -0,0 +1,7 @@
+import { SWDBAppConfig } from '@swdbapp/types';
+
+export const provideAssetsPath = (): string => (window as SWDBAppConfig).swDBAppConfig.assetsPath;
+
+export const provideAssetURL = (asset: string): URL => {
+    return new URL([provideAssetsPath(), asset].join('/'), window.origin);
+};
