0cb09dc73d20c20b8f1f552014590287c75c7614
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-07 01:18:12 +0200

    fix (/scripts/database.start.sh): + updated scripts folder + test right environment file

--- scripts/database.start.sh
+++ scripts/database.start.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-SCRIPTS_FOLDER="$(pwd)/db/scripts"
+SCRIPTS_FOLDER="$(pwd)/database/output";
+ENVIRONMENTS_FOLDER="$(pwd)/environments";
 
 if ! [ -d "${SCRIPTS_FOLDER}" ]; then
     echo "Could not found '${SCRIPTS_FOLDER}' directory!"
     exit 1
 fi
 
+if ! [ -d "${ENVIRONMENTS_FOLDER}" ]; then
+    echo "Could not found '${ENVIRONMENTS_FOLDER}' directory!"
+    exit 1
+fi
+
 docker run --name swdbapp-database \
-    --env-file "$(pwd)/.env" \
+    --env-file "${ENVIRONMENTS_FOLDER}/.env.local" \
     -p 5432:5432 \
-    -v "$(pwd)/db/scripts":/docker-entrypoint-initdb.d \
+    -v "${SCRIPTS_FOLDER}":/docker-entrypoint-initdb.d \
     -d postgres
