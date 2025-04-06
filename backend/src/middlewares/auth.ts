c5df2909b26c390839f39957faf5c006582588a7
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-06 11:20:24 +0200

    feature (/backend/**/auth.ts): improved middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,14 +1,22 @@
-import type { Request, Response, NextFunction } from "express";
-import { AppError } from "../types";
+import config from '../config';
+import type { Request, Response, NextFunction } from 'express';
+import { AppError } from '../types';
 
-export const auth = () => (req: Request, res: Response, next: NextFunction): void => {
-    const apiKeyHeaderName: string = 'X-API-KEY';
-    const apiKeyValue: string = `${process.env.API_KEY}`;
+export const auth =
+	() =>
+	(req: Request, res: Response, next: NextFunction): void => {
+		if (['OPTIONS'].includes(req.method)) {
+			res.statusCode = 200;
+			next();
+		} else {
+			const apiKeyHeaderName: string = 'x-api-key';
+			const apiKeyValue: string = `${config.apiKey}`;
 
-    const apiHeader = req.get(apiKeyHeaderName);
-    if (!apiHeader && apiHeader !== apiKeyValue) {
-        next({ status: 401, message: 'Unauthorized' } as AppError);
-    }
-
-    next();
-};
+			const apiHeader = req.get(apiKeyHeaderName);
+			if (!apiHeader && apiHeader !== apiKeyValue) {
+				next({ status: 401, message: 'Unauthorized' } as AppError);
+			} else {
+				next();
+			}
+		}
+	};
