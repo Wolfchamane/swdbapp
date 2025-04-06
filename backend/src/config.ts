5885d421d09cb5f4446763ce806307b410c52b80
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-06 11:20:51 +0200

    feature (/backend/src/config.ts): added apiKey from env variables

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,13 @@
 export interface AppConfiguration {
 	allowOrigin: string;
 	port: number;
+	apiKey: string;
 }
 
 const configuration: AppConfiguration = {
 	allowOrigin: `${process.env.BACKEND_ACCEPT_ORIGIN}`,
 	port: Number(process.env.BACKEND_PORT),
+	apiKey: `${process.env.VITE_API_KEY}`,
 };
 
 export default configuration;
