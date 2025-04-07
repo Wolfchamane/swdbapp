25937612d94ad20eb207bb9a6fbac42dc020637b
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-07 23:46:59 +0200

    fix (eras.route.ts): ts expect error

--- frontend/vue-js/src/router/routes/eras.route.ts
+++ frontend/vue-js/src/router/routes/eras.route.ts
@@ -3,6 +3,7 @@ import { faGalacticRepublic } from '@fortawesome/free-brands-svg-icons';
 export const ERA_ROUTE = {
 	name: 'eras-view',
 	path: '/eras',
+    // @ts-expect-error import
 	component: async () => await import('@/views/eras-view.vue'),
 	meta: {
 		icon: faGalacticRepublic,
@@ -12,5 +13,6 @@ export const ERA_ROUTE = {
 export const ERA_DETAILS_ROUTE = {
 	name: 'era-details-view',
 	path: '/eras/:id',
+    // @ts-expect-error import
 	component: async () => await import('@/views/era-details/era-details-view.vue'),
 };
