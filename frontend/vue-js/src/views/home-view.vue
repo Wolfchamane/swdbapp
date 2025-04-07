0f5561e7f15b02c8c3bb4eeb414a1fbd3c0a5579
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-07 23:44:22 +0200

    feature (home-view.vue): added disclaimer and welcome message

--- frontend/vue-js/src/views/home-view.vue
+++ frontend/vue-js/src/views/home-view.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup></script>
 
 <template lang="pug">
-    h1 Welcome!
+    .home-view.d-flex.flex-column.h-100.overflow-hidden
+        h1 Welcome!
+        .grow
+            p Use the icons on the left side to navigate between sections.
+        p.fs-s.p-05.ta-justify
+            | All data & pictures belongs to either LucasFilms Ltd.,
+            | The Walt Disney Company or their respective owners.
 </template>
