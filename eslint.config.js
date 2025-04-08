9243bd03cbc6bc2519647efdc6df657f8e9ab1cc
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-08 18:38:37 +0200

    chore (eslint): configure linter to not detect '_' arguments as no unused vars

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,17 @@
 import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ files: ['**/*.{js,mjs,cjs,ts}'] }, ...tseslint.configs.recommended];
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '_',
+				},
+			],
+		},
+	},
+];
