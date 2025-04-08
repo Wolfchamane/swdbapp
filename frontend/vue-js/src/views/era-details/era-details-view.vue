71b953c3ae981998779d37f589f3bcba199d7f25
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-08 18:01:51 +0200

    feature (/frontend/vue-js): link era-details view with title-details view

--- frontend/vue-js/src/views/era-details/era-details-view.vue
+++ frontend/vue-js/src/views/era-details/era-details-view.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 	import { type Ref, inject, ref, watch } from 'vue';
-	import { type RouteLocation, useRoute } from 'vue-router';
-	import { type EraDetails, type ErasUseCases, provideErasUseCases } from '@swdbapp/eras-feature';
+	import { type RouteLocation, useRoute, useRouter } from 'vue-router';
+	import { type EraDetails, type EraTitle, type ErasUseCases, provideErasUseCases } from '@swdbapp/eras-feature';
 	import type { Nullable } from '@swdbapp/types';
 
+    const ROUTER = useRouter();
 	const useCases: ErasUseCases = provideErasUseCases();
 	const currentRoute: RouteLocation = useRoute();
 	const toggleLoading: () => void = inject('toggleLoading');
@@ -17,6 +18,14 @@
 		toggleLoading();
 	};
 
+    const navigateToTitle = (id: number): void => {
+        ROUTER.push({ name: 'title-details-view', params: { id: `${id}` }});
+    };
+
+    const isTitleClickable = (title: EraTitle): boolean => {
+        return [33].includes(title.$id);
+    };
+
 	watch(
 		() => currentRoute.params.id,
 		eraId => fetchEraDetails(eraId as string),
@@ -25,8 +34,8 @@
 </script>
 
 <template lang="pug">
-    .era-details-view.p-relative
-        h1.era-details-view__header.d-flex.align-center.border-bottom-1.p-sticky.mt-0.bg-color-foreground
+    .era-details-view.p-relative.p-1
+        h1.era-details-view__header.d-flex.align-center.border-bottom-1.p-sticky.mt-0.bg-color-background.top-0
             img.w-2.h-2.mb-05.mr-1(:src='era?.logo.href', :alt='era?.name')/
             span {{ era?.name }}
 
@@ -37,7 +46,9 @@
             p.text-uppercase.border-bottom-1.pb-05.mt-2 Titles
 
             .d-flex.flex-column.center
-                img(v-for='title in era?.titles', :key='`era-title-${title.$id}`', :src='title.logo.href', :alt='title.name')/
+                img.era-details-view__title-logo(v-for='title in era?.titles', :key='`era-title-${title.$id}`', :src='title.logo.href', :alt='title.name',
+                :class='{ "cursor-pointer": isTitleClickable(title) }'
+                @click='navigateToTitle(title.$id)')/
 </template>
 
-<style lang="sass" src="./styles.sass"></style>
+<style lang='sass' src='./styles.sass'></style>
