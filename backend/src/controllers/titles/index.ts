1ca14a8735a16b0df853950b749450a2c9efaa3d
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-10 12:43:13 +0200

    feature (/backend): titles list controller

--- backend/src/controllers/titles/index.ts
+++ backend/src/controllers/titles/index.ts
@@ -2,3 +2,4 @@ import { selectOneByTitle } from './_queries';
 
 export { selectOneByTitle };
 export { default as titlesDetailController } from './detail';
+export { default as titlesListController } from './list';
--- backend/src/routes/titles.ts
+++ backend/src/routes/titles.ts
@@ -1,6 +1,7 @@
-import { titlesDetailController } from '../controllers/titles';
+import { titlesDetailController, titlesListController } from '../controllers/titles';
 import type { Router } from 'express';
 
 export default (router: Router) => {
+	router.get('/api/titles', titlesListController);
 	router.get('/api/titles/:id', titlesDetailController);
 };
