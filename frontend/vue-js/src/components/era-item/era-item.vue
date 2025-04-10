ef02346aaf20af6d36a62641767062ea9764406c
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-10 12:51:55 +0200

    ref (/vue-js/**/era-item): remove styles and pug syntax

--- frontend/vue-js/src/components/era-item/era-item.vue
+++ frontend/vue-js/src/components/era-item/era-item.vue
@@ -4,10 +4,9 @@
 	defineProps<EraItemProperties>();
 </script>
 
-<template lang="pug">
-    .era-item.d-flex.align-center.border-1.radius-s.my-1.cursor-pointer
-        img.era-item__icon.w-2.h-2.mb-05.ml-1.mr-2(:src='era.logo.href', :alt='era.name')/
-        span.era-item__name {{ era.name }}
+<template>
+	<div class="list-item d-flex align-center border-1 radius-s my-1 cursor-pointer">
+		<img class="era-item__icon w-2 h-2 mb-05 ml-1 mr-2" :src="era.logo.href" :alt="era.name" />
+		<span class="era-item__name">{{ era.name }}</span>
+	</div>
 </template>
-
-<style lang="sass" src="./styles.sass"></style>
