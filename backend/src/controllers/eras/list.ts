eeed3871151871a5172557cc775e2e6a03b72d56
Wolfchamane <arturo.martinez@amartinez.dev>
2025-04-10 12:42:47 +0200

    feature (/backend): update eras list controller

--- backend/src/controllers/eras/list.ts
+++ backend/src/controllers/eras/list.ts
@@ -1,56 +1,28 @@
-import type { EraModel, ListOutput } from '@swdbapp/infra-http';
+import type { EraModel } from '@swdbapp/infra-http';
 import type { Request, Response, NextFunction } from 'express';
 import { query } from '../../db';
 import { selectAll, count } from './_queries';
 import type { QueryResult, QueryConfig } from 'pg';
-import { DEFAULT_LIMIT, DEFAULT_ORDER_BY, DEFAULT_ORDER_DIR, DEFAULT_OFFSET } from '../../constants';
-import type { AppResponse, AppError, SelectAllInput } from '../../types';
-import { log, debug, error } from '../../log';
+import type { SelectAllInput } from '../../types';
+import { debug } from '../../log';
+import { listController } from '../common';
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-	log('<-- Handling: [%s] %s', req.method, req.url);
-
-	const limit: string = `${req.query?.limit || DEFAULT_LIMIT}`;
-	const offset: string = `${req.query?.offset || DEFAULT_OFFSET}`;
-	const orderBy: string = `${req.query?.orderBy || DEFAULT_ORDER_BY}`;
-	const orderDir: string = `${req.query?.orderDir || DEFAULT_ORDER_DIR}`;
-	const searchBy: string = `${req.query?.searchBy || ''}`;
-	const search: string = `${req.query?.search || ''}`;
-
-	const selectAllInput: SelectAllInput = { limit, orderBy, orderDir, searchBy, search, offset };
-	debug('select all input: %s', JSON.stringify(selectAllInput));
-
-	try {
-		const querySelectAllConfig: QueryConfig = await selectAll(selectAllInput);
-		debug('querySelectAllConfig: %s', JSON.stringify(querySelectAllConfig));
+	const queryAll = async (input: SelectAllInput): Promise<EraModel[]> => {
+		const querySelectAllConfig: QueryConfig = await selectAll(input);
+		debug('query all eras config: %s', JSON.stringify(querySelectAllConfig));
 		const selectResponse: QueryResult<EraModel> = await query(querySelectAllConfig);
-		debug('select query performed!');
 
+		return selectResponse.rows.slice();
+	};
+
+	const queryCount = async (): Promise<number> => {
 		const queryCountConfig: QueryConfig = await count();
-		debug('queryCountConfig: %s', JSON.stringify(queryCountConfig));
+		debug('query count eras config: %s', JSON.stringify(queryCountConfig));
 		const countResponse: QueryResult = await query(queryCountConfig);
-		debug('count performed!');
 
-		const items = selectResponse.rows.slice();
-		const total = Number(countResponse.rows[0].count);
+		return Number(countResponse.rows[0].count);
+	};
 
-		const listOutput: ListOutput<EraModel> = {
-			offset: Number(offset),
-			limit: Number(limit),
-			total,
-			items,
-		};
-
-		if (!total) {
-			error('No items found!');
-		}
-
-		next({
-			status: 200,
-			message: JSON.stringify(listOutput),
-		} as AppResponse);
-	} catch (e: unknown) {
-		error((e as Error).message);
-		next(e as AppError);
-	}
+	return await listController<EraModel>({ req, res, next, queryAll, queryCount });
 };
