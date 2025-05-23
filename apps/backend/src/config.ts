580ee618b83c7fafde8b2ad7fba7082c792bae7c
Wolfchamane <arturo.martinez@amartinez.dev>
2025-05-24 00:47:43 +0200

    chore (*): revert configuration test logs
    [NO-REF]

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -10,10 +10,9 @@ const configuration: AppConfiguration = {
 	apiKey: `${process.env.VITE_API_KEY}`,
 };
 
-// const mode: string = process.env.NODE_ENV || process.env.MODE || '';
-// if (/^dev/gi.test(mode)) {
-// 	console.log(`[@swdbapp/backend] Configuration: ${JSON.stringify(configuration)}`);
-// }
-console.log(`[@swdbapp/backend] Configuration: ${JSON.stringify(configuration)}`);
+const mode: string = process.env.NODE_ENV || process.env.MODE || '';
+if (/^dev/gi.test(mode)) {
+	console.log(`[@swdbapp/backend] Configuration: ${JSON.stringify(configuration)}`);
+}
 
 export default configuration;
--- utils/backend/db/index.ts
+++ utils/backend/db/index.ts
@@ -1,20 +1,25 @@
 import { Pool, type PoolConfig, type QueryConfig, type QueryResult } from 'pg';
 
+const user: string = `${process.env.PGUSER}`;
+const host: string = `${process.env.PGHOST}`;
+const database: string = `${process.env.PGDATABASE}`;
+const password: string = `${process.env.PGPASSWORD}`;
+const port: number = Number(process.env.PGPORT);
+const connectionString: string = `postgres://${user}:${password}@${host}:${port}/${database}`;
+
 const config: PoolConfig = {
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	database: process.env.PGDATABASE,
-	password: process.env.PGPASSWORD,
-	port: Number(process.env.PGPORT),
+	user,
+	host,
+	database,
+	password,
+	port,
+	connectionString,
 };
-config.connectionString = `postgres://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`;
 
-// const mode: string = process.env.NODE_ENV || process.env.MODE || '';
-// if (/^dev/gi.test(mode)) {
-// 	console.log('[@swdbapp/backend] DB pool configuration: %s', JSON.stringify(config));
-// }
-
-console.log('[@swdbapp/backend] DB pool configuration: %s', JSON.stringify(config));
+const mode: string = process.env.NODE_ENV || process.env.MODE || '';
+if (/^dev/gi.test(mode)) {
+	console.log('[@swdbapp/backend] DB pool configuration: %s', JSON.stringify(config));
+}
 
 const pool: Pool = new Pool(config);
 
