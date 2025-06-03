30372f8f6c21d992cf47265b3a6b8278239951c9
Wolfchamane <arturo.martinez@amartinez.dev>
2025-06-03 18:42:29 +0200

    feat (/apps/backend/*.ts): define application logging level from env variable
    [NO-REF]

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,8 +3,9 @@ import { auth, cors, json, endHandler } from './middlewares';
 import provideRouter from './routes';
 import { DefaultLogger } from '@swdbapp/utils-backend';
 import { name, version } from '../package.json';
+import config from './config';
 
-const logger = DefaultLogger.$i({ module: name, version });
+const logger = DefaultLogger.$i({ module: name, version, level: config.logLevel });
 
 const app = express();
 
--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,13 +1,17 @@
+import { type LoggerLevel, LOGGER_LEVELS } from '@swdbapp/utils-backend';
+
 export interface AppConfiguration {
 	allowOrigin: string;
 	port: number;
 	apiKey: string;
+	logLevel: LoggerLevel;
 }
 
 const configuration: AppConfiguration = {
 	allowOrigin: `${process.env.BACKEND_ACCEPT_ORIGIN}`,
 	port: Number(process.env.BACKEND_PORT),
 	apiKey: `${process.env.VITE_API_KEY}`,
+	logLevel: (`${process.env.BACKEND_LOG_LEVEL}` || LOGGER_LEVELS.DEBUG) as LoggerLevel,
 };
 
 const mode: string = process.env.NODE_ENV || process.env.MODE || '';
