df3718a52a74ecaf5c1a5d7c9f3b05be51186dbc
Wolfchamane <arturo.martinez@amartinez.dev>
2025-06-10 09:50:05 +0200

    chore (*/app-menu-item/*): removed
    [NO-REF]

--- apps/frontend/vue-js/src/components/app-menu-item/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './types';
-export { default as AppMenuItem } from './app-menu-item.vue';
--- apps/frontend/vue-js/src/components/app-menu-item/types.ts
+++
@@ -1,9 +0,0 @@
-import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-
-export interface AppMenuItemProperties {
-	title: string;
-	icon?: IconDefinition;
-	query?: Record<string, string | number>;
-	disabled?: boolean;
-	active: boolean;
-}
