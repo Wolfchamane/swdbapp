11c85f19b064ac582f773216a452df79e0edf902
Wolfchamane <arturo.martinez@amartinez.dev>
2025-06-10 09:47:05 +0200

    feature (/libs/**/vue-js/**/list-item*): render image if set
    [NO-REF]

--- libs/frontend/components/vue-js/src/list-item/list-item.types.ts
+++ libs/frontend/components/vue-js/src/list-item/list-item.types.ts
@@ -1,4 +1,5 @@
 export interface ListItemProperties {
 	image?: string;
 	label: string;
+	active?: boolean;
 }
--- libs/frontend/components/vue-js/src/list-item/list-item.vue
+++ libs/frontend/components/vue-js/src/list-item/list-item.vue
@@ -7,9 +7,19 @@
 </script>
 
 <template>
-	<div class="list-item d-flex align-center border-1 radius-s my-1 cursor-pointer">
-		<img class="era-item__image w-2 h-2 mb-05 mx-1" :src="image" :alt="label" />
-		<span class="era-item__name grow">{{ label }}</span>
-		<font-awesome-icon class="list-item__icon mx-1" :icon="faCircleRight" />
+	<div
+		:class="[
+			'list-item',
+			'd-flex',
+			'align-center',
+			'border-1',
+			'radius-s',
+			'my-1',
+			'cursor-pointer',
+			{ 'list-item--active': active },
+		]">
+		<img v-if="image" class="era-item__image w-2 h-2 mb-05 mx-1" :src="image" :alt="label" />
+		<span class="ml-1 era-item__name grow">{{ label }}</span>
+		<font-awesome-icon v-show="!active" class="list-item__icon mx-1" :icon="faCircleRight" />
 	</div>
 </template>
