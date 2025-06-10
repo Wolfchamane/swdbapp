50c21880d3597efdc9046a5e68368d77ef978dd9
Wolfchamane <arturo.martinez@amartinez.dev>
2025-06-10 09:52:03 +0200

    ref (/apps/**/vue-js/**/home-view.vue): remove content
    [NO-REF]

--- apps/frontend/vue-js/src/views/home-view.vue
+++ apps/frontend/vue-js/src/views/home-view.vue
@@ -1,11 +1,4 @@
 <script lang="ts" setup></script>
 
 <template lang="pug">
-    .home-view.d-flex.flex-column.h-100.overflow-hidden.p-1
-        h1 Welcome!
-        .grow
-            p Use the icons on the left side to navigate between sections.
-        p.fs-s.p-05.ta-justify
-            | All data & pictures belongs to either LucasFilms Ltd.,
-            | The Walt Disney Company or their respective owners.
 </template>
